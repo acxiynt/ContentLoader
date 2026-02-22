548ff4abca52cce713f339b01dbf927e6d2fb49c
acxiynt <3059872@gmail.com>
2026-02-22 15:19:31 +0900

    disgusting but it worked

--- Main.cs
+++ Main.cs
@@ -5,6 +5,8 @@ using HarmonyLib;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 namespace Genesis.ContentLoader;
 
 public class Main : IPluginBase
@@ -19,20 +21,18 @@ public class Main : IPluginBase
             typeof(Tables).GetConstructor(new Type[] { typeof(Func<string, JSONNode>) }),
             prefix: new HarmonyMethod(method: typeof(Patch).GetMethod(nameof(Patch._Tables)))
         );
+        IEnumerable<Type> tableTypes = typeof(Tables).GetProperties(BindingFlags.Public | BindingFlags.Instance).Select(property => property.PropertyType);
+        IEnumerable<ConstructorInfo> ctors = tableTypes.Select(tableTypes => tableTypes.GetConstructor(new Type[] { typeof(JSONNode) })).Where(ctor => ctor != null);
+        foreach (ConstructorInfo ctor in ctors)
+        {
+            harmony.Patch(ctor, null, null, new HarmonyMethod(method: typeof(Patch).GetMethod(nameof(Patch._Ctor))));
+        }
         Util.LogString("ContentLoader", "Patch finished");
-    }
-
-    public void OnGameInit()
-    {
     }
 }
 
 public class Patch
 {
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(Tables))]
-    [HarmonyPatch(MethodType.Constructor)]
-    [HarmonyPatch(new Type[] { typeof(Func<string, JSONNode>) })]
     public static void _Tables(ref Func<string, JSONNode> loader)
     {
         Func<string, JSONNode> _loader = loader;
@@ -70,4 +70,35 @@ public class Patch
             }
         };
     }
+
+    public static IEnumerable<CodeInstruction> _Ctor(IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (CodeInstruction il in instructions)
+        {
+            if (il.opcode == OpCodes.Callvirt &&
+                il.operand is MethodInfo method &&
+                method.Name == "Add" &&
+                method.DeclaringType.IsGenericType &&
+                method.DeclaringType.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+            {
+                MethodInfo setter = method.DeclaringType.GetProperty("Item").GetSetMethod();
+                yield return new CodeInstruction(OpCodes.Callvirt, setter);
+            }
+            else
+            {
+                yield return il;
+            }
+        }
+    }
 }
+
+/*
+IL_0049: callvirt instance string DolocTown.Config.Resource.VegetationSpawnLut::get_Id()
+
+IL_004e: ldloc.1
+IL_004f: callvirt instance void class [netstandard] System.Collections.Generic.Dictionary`2<string, class DolocTown.Config.Resource.VegetationSpawnLut>::Add(!0, !1)
+IL_0049: callvirt instance string DolocTown.Config.Time.ChairProto::get_Id()
+
+IL_004E: ldloc.1
+IL_004F: callvirt instance void class [mscorlib] System.Collections.Generic.Dictionary`2<string, class DolocTown.Config.Time.ChairProto>::set_Item(!0, !1)
+*/
